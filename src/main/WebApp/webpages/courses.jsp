ceec67074b6cc4ec0cb1ec5f14e5b2c78f218ea6
sagarmohanty <sagar.mohanty@qualibar.com>
2023-02-16 14:19:30 -0800

    queryParam

--- src/main/WebApp/webpages/courses.jsp
+++ src/main/WebApp/webpages/courses.jsp
@@ -7,7 +7,8 @@
 <title>Insert title here</title>
 </head>
 <body>
-Hi
+
+Welcome to courses ! // ${cName}
 
 </body>
 </html>
--- src/main/java/com/example/demo/controller/CoursesController.java
+++ src/main/java/com/example/demo/controller/CoursesController.java
@@ -3,11 +3,17 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class CoursesController {
-	@RequestMapping("courses")
-	public String courses() {
-		System.out.println("My courses");
+	@RequestMapping("/courses")
+	public String courses(HttpServletRequest req) {
+		HttpSession session = req.getSession();
+		String cName = req.getParameter("cName");
+		System.out.println("My courses: " + cName);
+		session.setAttribute("cName", cName);
 		return "courses";
 	}
 
